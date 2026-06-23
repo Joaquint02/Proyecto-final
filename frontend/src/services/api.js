1d8e95f2f6936962e61d72ddebd897644c638353
Joaquint02 <joaquin.trias@anima.edu.uy>
2026-06-23 20:21:01 -0300

    actualizacion

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/products",
+  baseURL: "http://localhost:3000/api/productos",
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
 
   if (token) {
-    config.headers.Authorization =
-      `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
